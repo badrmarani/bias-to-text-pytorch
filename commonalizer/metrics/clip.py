88843c64aea7be1bacf0966f1b2afa4de2f00133
badrmarani <badr-eddine.marani@outlook.com>
2023-10-19 15:57:53 +0200

    add `git_score`

--- commonalizer/metrics/clip.py
+++ commonalizer/metrics/clip.py
@@ -5,8 +5,60 @@ import skimage.io as io
 import torch
 from PIL import Image
 from tqdm import trange
+from transformers import AutoModel, AutoProcessor, AutoTokenizer
 
 from .. import seed_everything
+
+
+def mean_pooling(model_output, attention_mask):
+    # first element of model_output contains all token embeddings
+    token_embeddings = model_output[0]
+    input_mask_expanded = (
+        attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    )
+    sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
+    sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+    return sum_embeddings / sum_mask
+
+
+@seed_everything()
+@torch.no_grad()
+def git_score(root, filenames, keywords, device, git_weights_path):
+    images = [os.path.join(root, f) for f in filenames]
+    images = [Image.fromarray(io.imread(im)) for im in images]
+
+    processor = AutoProcessor.from_pretrained("microsoft/git-base")
+    model = AutoModel.from_pretrained("microsoft/git-base")
+    tokenizer = AutoTokenizer.from_pretrained("microsoft/git-base")
+
+    similarity_list = []
+
+    encoded_input = tokenizer(
+        [f"a photo of a {c}" for c in keywords],
+        padding=True,
+        truncation=True,
+        max_length=128,
+        return_tensors="pt",
+    )
+
+    model_output = model(**encoded_input)
+    text_inputs = mean_pooling(model_output, encoded_input["attention_mask"])
+
+    num_steps = len(images) // 10 if len(images) > 10 else len(images)
+    for i in trange(0, len(images), num_steps):
+        batch = processor(
+            images=images[i : i + num_steps], return_tensors="pt"
+        ).pixel_values
+
+        image_inputs = model.image_encoder(batch).last_hidden_state.mean(dim=1)
+
+        image_features = image_inputs / image_inputs.norm(dim=-1, keepdim=True)
+        text_features = text_inputs / text_inputs.norm(dim=-1, keepdim=True)
+        similarity = 100.0 * image_features @ text_features.T
+        similarity_list += [similarity]
+
+    similarity = torch.cat(similarity_list).mean(dim=0)
+    return similarity
 
 
 @seed_everything()
@@ -21,7 +73,10 @@ def clip_score(root, filenames, keywords, device, clip_weights_path):
 
     similarity_list = []
 
-    # batchs = [images[i : i + 2000] for i in range(0, len(images), 2000)]
+    text_inputs = torch.cat(
+        [clip.tokenize(f"a photo of a {c}") for c in keywords]
+    ).to(device=device)
+
     num_steps = len(images) // 10 if len(images) > 10 else len(images)
     for i in trange(0, len(images), num_steps):
         batch = images[i : i + num_steps]
@@ -29,10 +84,6 @@ def clip_score(root, filenames, keywords, device, clip_weights_path):
         # prepare the inputs
         image_inputs = torch.cat(
             [preprocess(pil_image).unsqueeze(0) for pil_image in batch]
-        ).to(device=device)
-
-        text_inputs = torch.cat(
-            [clip.tokenize(f"a photo of a {c}") for c in keywords]
         ).to(device=device)
 
         # compute features
